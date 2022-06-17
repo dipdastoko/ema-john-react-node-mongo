52541a58f28997c38df8e09405feff4c7046ad4c
dipdastoko <dipdastoko@gmail.com>
2022-06-18 02:17:48 +0600

    pagination created

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -9,6 +9,8 @@ import { Link } from 'react-router-dom';
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useCart(products);
+    const [page, setPage] = useState(0);
+    const [pageCount, setPageCount] = useState(0);
     // products to be rendered on the UI
     const [displayProducts, setDisplayProducts] = useState([]);
 
@@ -16,8 +18,11 @@ const Shop = () => {
         fetch('http://localhost:5000/products')
             .then(res => res.json())
             .then(data => {
-                setProducts(data);
-                setDisplayProducts(data);
+                setProducts(data.products);
+                setDisplayProducts(data.products);
+                const count = data.count;
+                const pageNumber = Math.ceil(count / 10);
+                setPageCount(pageNumber);
             });
     }, []);
 
@@ -67,6 +72,15 @@ const Shop = () => {
                         >
                         </Product>)
                     }
+                    <div className="pagination">
+                        {
+                            [...Array(pageCount).keys()].map(number => <button
+                                className={number === page ? 'selected' : ''}
+                                key={number}
+                                onClick={() => setPage(number)}
+                            >{number}</button>)
+                        }
+                    </div>
                 </div>
                 <div className="cart-container">
                     <Cart cart={cart}>
